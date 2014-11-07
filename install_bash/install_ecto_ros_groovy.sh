1e25b0a956eefdd6419c9c90010685a7a9b45bf1
sbhle <boehle@gmail.com>
2014-11-07 16:28:24 +0100

    Added sh to install ecto without pipelines

---
+++ install_bash/install_ecto_ros_groovy.sh
@@ -0,0 +1,9 @@
+echo "Installing ROS ORK without pipelines ..."
+sudo apt-get install ros-groovy-object-recognition-ros
+sudo apt-get install ros-groovy-object-recognition-core
+sudo apt-get install ros-groovy-object-recognition-msgs
+sudo apt-get install ros-groovy-object-recognition-reconstruct*
+sudo apt-get install ros-groovy-object-recognition-renderer
+sudo apt-get install ros-groovy-object-recognition-capture
+
+echo "Feel free to install ORK pipelines now!"
